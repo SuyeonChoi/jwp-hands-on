da3fc5c7526ab8d132b8533523bc319e7dab0d95
SuyeonChoi <tndus970120@gmail.com>
2022-09-28 21:26:45 +0900

    feat: 4단계 주석 추가 및 ConsumerWrapper와 FunctionWrapper를 사용하여 코드 개선

--- di/src/test/java/nextstep/study/di/stage4/annotations/DIContainer.java
+++ di/src/test/java/nextstep/study/di/stage4/annotations/DIContainer.java
@@ -2,11 +2,12 @@ package nextstep.study.di.stage4.annotations;
 
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
-import java.lang.reflect.InvocationTargetException;
 import java.util.Arrays;
 import java.util.NoSuchElementException;
 import java.util.Set;
 import java.util.stream.Collectors;
+import nextstep.study.ConsumerWrapper;
+import nextstep.study.FunctionWrapper;
 
 /**
  * 스프링의 BeanFactory, ApplicationContext에 해당되는 클래스
@@ -21,60 +22,46 @@ class DIContainer {
     }
 
     public static DIContainer createContainerForPackage(final String rootPackageName) {
-        final Set<Class<?>> classes = ClassPathScanner.getAllClassesInPackage(rootPackageName)
+        return ClassPathScanner.getAllClassesInPackage(rootPackageName)
                 .stream()
                 .filter(DIContainer::filterServiceAndRepositoryAnnotation)
-                .collect(Collectors.toSet());
-
-        return new DIContainer(classes);
+                .collect(Collectors.collectingAndThen(Collectors.toSet(), DIContainer::new));
     }
 
     private static boolean filterServiceAndRepositoryAnnotation(final Class<?> aClass) {
         return aClass.isAnnotationPresent(Service.class) || aClass.isAnnotationPresent(Repository.class);
     }
 
+    // 기본 생성자로 빈을 생성한다.
     private Set<Object> createBeans(final Set<Class<?>> classes) {
         return classes.stream()
-                .map(this::createInstance)
+                .map(FunctionWrapper.apply(Class::getDeclaredConstructor))
+                .peek(it -> it.setAccessible(true))
+                .map(FunctionWrapper.apply(Constructor::newInstance))
                 .collect(Collectors.toSet());
     }
 
-    private Object createInstance(final Class<?> beanClass) {
-        try {
-            final Constructor<?> constructor = beanClass.getDeclaredConstructor();
-            constructor.setAccessible(true);
-            return constructor.newInstance();
-        } catch (final InstantiationException | IllegalAccessException | InvocationTargetException |
-                       NoSuchMethodException e) {
-            throw new RuntimeException("fail to create instance", e);
-        }
-    }
-
+    // 빈 내부에 선언된 필드를 각각 셋팅한다.
     private void setFields(final Object bean) {
         Arrays.stream(bean.getClass().getDeclaredFields())
                 .filter(it -> it.isAnnotationPresent(Inject.class))
-                .forEach(it -> initializeField(bean, it));
-
+                .forEach(it -> setField(bean, it));
     }
 
-    private void initializeField(final Object bean, final Field field) {
-        for (final Object b : beans) {
-            field.setAccessible(true);
-            final Class<?> fieldType = field.getType();
-            if (fieldType.isAssignableFrom(b.getClass())) {
-                try {
-                    field.set(bean, b);
-                } catch (final IllegalAccessException e) {
-                    throw new RuntimeException(e);
-                }
-            }
-        }
+    // 각 필드에 빈을 대입(assign)한다.
+    private void setField(final Object bean, final Field field) {
+        field.setAccessible(true);
+        final Class<?> fieldType = field.getType();
+
+        beans.stream()
+                .filter(fieldType::isInstance)
+                .forEach(ConsumerWrapper.accept(matchBean -> field.set(bean, matchBean)));
     }
 
     @SuppressWarnings("unchecked")
     public <T> T getBean(final Class<T> aClass) {
         return (T) beans.stream()
-                .filter(it -> aClass.isAssignableFrom(it.getClass()))
+                .filter(aClass::isInstance)
                 .findFirst()
                 .orElseThrow(() -> new NoSuchElementException("fail to find bean"));
     }
